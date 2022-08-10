bcefdef5d925872c6bcc39ea300fbbe37cf1de22
jpoh281 <hjp4869@naver.com>
2022-08-10 23:17:47 +0900

    ✨현재 시간 매개변수로 보내기

--- CriminalItent/app/src/main/java/com/hdd/criminalitent/DatePickerFragment.kt
+++ CriminalItent/app/src/main/java/com/hdd/criminalitent/DatePickerFragment.kt
@@ -2,14 +2,19 @@ package com.hdd.criminalitent
 
 import android.app.DatePickerDialog
 import android.app.Dialog
-import android.icu.util.Calendar
+
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.navArgs
+import java.util.*
 
 class DatePickerFragment : DialogFragment() {
 
+    private  val args: DatePickerFragmentArgs by navArgs()
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val calendar = Calendar.getInstance()
+        calendar.time = args.crimeDate
         val initYear = calendar.get(Calendar.YEAR)
         val initMonth = calendar.get(Calendar.MONTH)
         val initDay = calendar.get(Calendar.DAY_OF_MONTH)
